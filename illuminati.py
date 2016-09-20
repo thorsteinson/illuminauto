64261bc356ea15043eb3ea81d9106332fc418bed
Jole <hksjn@slipry.net>
2016-09-20 10:34:12 -0700

    Uses better image downscaling

--- illuminati.py
+++ illuminati.py
@@ -1,21 +1,26 @@
 import numpy as np
 from sklearn.cluster import MeanShift, estimate_bandwidth
 from skimage.data import load
-from skimage.transform import resize
+from skimage.measure import block_reduce
+from skimage.transform import resize, rescale
 from skimage.color import rgb2gray
-from colorsys import rgb_to_hsl
+from colorsys import rgb_to_hls
 
 import time # For performance measurment
+import os
+from os.path import join # For file access
 
-TEST_IMAGE_PATH = "/users/caleb/src/illuminati/test_desktop.png"
-IMAGE_DIMENSION = 64
+TEST_IMAGE_PATH = join(os.getcwd(), "test_image.jpg")
+BLOCK_SIZE = 32
 
 lizard = load(TEST_IMAGE_PATH)
 
 # Takes an image as a numpy array, and returns the color of the primary cluster
 def color_extract(img):
     # Resize the image
-    mini_img = resize(img, (IMAGE_DIMENSION, IMAGE_DIMENSION))
+    mini_img = block_reduce(img, block_size=(BLOCK_SIZE, BLOCK_SIZE, 1), func=np.mean)
+    # Normalize
+    mini_img = mini_img / 255
 
     # Perform clustering
     ## Reshapes the array so that it's a single array of pixels
@@ -30,7 +35,7 @@ def color_extract(img):
 
     ## Note, we should do a check on the L param to see whether the color is close or not to white / black.
     ## We can set a cutoff point of 20% either way, and that will make the primary for lights either "bright" or "dim"
-    colors = [rgb_vec_to_hsl(c) for c in colors]
+    colors = [rgb_vec_to_hls(c) for c in colors]
 
     # Get the luminance
     gray = rgb2gray(mini_img)
@@ -43,16 +48,16 @@ def color_extract(img):
     print("Luminance")
     print(luminance)
 
-# Converts RGB vector into an HSL tuple (scaled to 255)
-def rgb_vec_to_hsl(v):
+# Converts RGB vector into an HLS tuple (scaled to 255)
+def rgb_vec_to_hls(v):
     r = v[0]
     g = v[1]
     b = v[2]
-    t = rgb_to_hsl(r, g, b)
+    t = rgb_to_hls(r, g, b)
     return (t[0] * 255, t[1] * 255, t[2] * 255)
 
-start = time.perf_counter()
+start = time.process_time()
 color_extract(lizard)
-end = time.perf_counter()
+end = time.process_time()
 
 print("Total time: %s", end - start)
