ea9b4d400b341ef79c9c9536e34ab50c08ae1de3
Caleb Thorsteinson <caleb@thorsteinson.io>
2016-09-21 14:08:01 -0700

    Refactors light into separate class

--- illuminauto.py
+++ illuminauto.py
@@ -1,21 +1,13 @@
+from lightcalc import LightCalc
 import numpy as np
-from sklearn.cluster import MeanShift, estimate_bandwidth
-from skimage.data import load
-from skimage.measure import block_reduce
-from skimage.color import rgb2gray
-from colorsys import rgb_to_hls
-from urllib.parse import urljoin
 import posixpath
 import requests
 import json
-
 from PIL import ImageGrab
-
 import time # For performance measurment
 import os
 from os.path import join # For file access
 
-BLOCK_SIZE = 32
 PERIOD = 2 # Seconds between runs
 PHILLIPS_HUE_MAX_BRIGHTNESS = 254
 PHILLIPS_HUE_MAX_HUE = 65535
@@ -30,57 +22,13 @@ PHILLIPS_HUE_SECONDARY_LIGHT_GROUP_ID = "3"
 # (Not sure how multiple desktops work...)
 def take_screenshot(): return np.array(ImageGrab.grab())
 
-# Takes an image as a numpy array, and returns the color of the primary cluster
-def color_extract(img):
-    # Resize the image
-    mini_img = block_reduce(img, block_size=(BLOCK_SIZE, BLOCK_SIZE, 1), func=np.mean)
-    # Normalize
-    mini_img = mini_img / 255
-
-    # Perform clustering
-    ## Reshapes the array so that it's a single array of pixels
-    img_points = mini_img.reshape(mini_img.shape[0] * mini_img.shape[1], 3)
-    bandwidth = estimate_bandwidth(img_points, quantile=0.2, n_samples=500)
-    model = MeanShift(bandwidth=bandwidth, bin_seeding=True)
-    model.fit(img_points)
-
-    clusters = model.cluster_centers_
-    # Grab the first two colors
-    colors = clusters[:2].tolist()
-
-    ## Note, we should do a check on the L param to see whether the color is close or not to white / black.
-    ## We can set a cutoff point of 20% either way, and that will make the primary for lights either "bright" or "dim"
-    colors = [rgb_vec_to_hls(c) for c in colors]
-    for c in colors:
-        amplify(c)
-        
-    # Get the luminance
-    gray = rgb2gray(mini_img)
-    luminance = np.average(gray)
-
-    # Return the hue and luminance
-    return {
-        'colors': colors,
-        'brightness': luminance
-    }
-
-# Converts RGB vector into easier to manipulate HLS color map
-def rgb_vec_to_hls(v):
-    r = v[0]
-    g = v[1]
-    b = v[2]
-    t = rgb_to_hls(r, g, b)
-    return {'hue': t[0],
-            'lightness': t[1],
-            'sat': t[2]}
-
 def run_forever(freq):
     while True:
         start = time.perf_counter()
         try:
-            light_calc = color_extract(take_screenshot())
+            calc = LightCalc(take_screenshot())
             print(light_calc)
-            change_lights(light_calc)
+            change_lights(calc.colors, calc.brightness)
         except OSError as err:
             print("Couldn't caputure the screen, continuing...")
         end = time.perf_counter()
@@ -88,17 +36,6 @@ def run_forever(freq):
         # Sleep until the end of the next period
         total_time = end-start
         time.sleep(max(0, freq - total_time))
-
-# Amplifies a color so that it's saturation is amplified or cut off (becomes white)
-# Takes a color represented as a map
-def amplify(color):
-    lightness = color['lightness']
-    if lightness > 0.85:
-        # Just make the light totally white
-        color['sat'] = 0
-    else:
-        # Amplify the saturation to make it pop
-        color['sat'] = min(1, color['sat'] + 0.2)
 
 # Takes a color and brightness then returns a map that will act the request body to phillips hue
 def construct_hue_body(color, brightness):
@@ -122,9 +59,8 @@ def request_hue(color, brightness, group_id):
     r = requests.put(url, data=payload)
 
 # Makes a request to the phillips hue system
-def change_lights(light_calc):
+def change_lights(colors, brightness):
     # Get the colors
-    colors = light_calc['colors']
     primary_color = colors[0]
     if len(colors) < 2:
         # Make the primary and secondary colors the same
@@ -132,7 +68,7 @@ def change_lights(light_calc):
     else:
         secondary_color = colors[1]
     
-    request_hue(primary_color, light_calc['brightness'], PHILLIPS_HUE_PRIMARY_LIGHT_GROUP_ID)
-    request_hue(secondary_color, light_calc['brightness'], PHILLIPS_HUE_SECONDARY_LIGHT_GROUP_ID)
+    request_hue(primary_color, brightness, PHILLIPS_HUE_PRIMARY_LIGHT_GROUP_ID)
+    request_hue(secondary_color, brightness, PHILLIPS_HUE_SECONDARY_LIGHT_GROUP_ID)
 
 run_forever(PERIOD)
---
+++ lightcalc.py
@@ -0,0 +1,74 @@
+import numpy as np
+from sklearn.cluster import MeanShift, estimate_bandwidth
+from skimage.data import load
+from skimage.measure import block_reduce
+from skimage.color import rgb2gray
+from colorsys import rgb_to_hls
+
+class LightCalc:
+    """A class that takes an image and extracts the main colors out of it"""
+    _DEFAULT_BLOCK_SIZE = 32
+
+    def __init__(self, img, **keywords):
+        img_dimension_size = len(img.shape)
+        if img_dimension_size != 3:
+            raise TypeError('Image dimension expected to be 3, found: ' + img_dimension_size)
+
+        img_pixel_size = img.shape[2]
+        if img_pixel_size != 3:
+            raise TypeError('Dimension for pixels expected to be 3, found: ' img_pixel_size)
+
+        blocksize = keywords.get('blocksize') if 'blocksize' in keywords else _DEFAULT_BLOCK_SIZE
+
+        mini_img = reduce_image(img, blocksize)
+
+        self.colors = calc_colors(mini_img)
+        self.brightness = luminance(mini_img)
+
+# Takes an image as a numpy array, and returns the color of the primary cluster
+# Resize the image
+def reduce_image(img, blocksize):
+    mini_img = block_reduce(img, block_size=(blocksize, blocksize, 1), func=np.mean)
+    # Normalize
+    return mini_img / 255
+
+# Performs clustering on an image and returns an array of the different centriods found
+def calc_colors(img):
+    ## Reshapes the array so that it's a single array of pixels
+    img_points = img.reshape(img.shape[0] * img.shape[1], 3)
+    bandwidth = estimate_bandwidth(img_points, quantile=0.2, n_samples=500)
+    model = MeanShift(bandwidth=bandwidth, bin_seeding=True)
+    model.fit(img_points)
+
+    colors = model.cluster_centers_.tolist()
+
+    colors = [rgb_vec_to_hls(c) for c in colors]
+    for c in colors:
+        amplify(c)
+    return colors
+
+# Returns the overall luminance of an image
+def calc_brightness(img):
+    gray = rgb2gray(mini_img)
+    return np.average(gray)
+
+# Converts RGB vector into easier to manipulate HLS color map
+def rgb_vec_to_hls(v):
+    r = v[0]
+    g = v[1]
+    b = v[2]
+    t = rgb_to_hls(r, g, b)
+    return {'hue': t[0],
+            'lightness': t[1],
+            'sat': t[2]}
+
+# Amplifies a color so that it's saturation is amplified or cut off (becomes white)
+# Takes a color represented as a map
+def amplify(color):
+    lightness = color['lightness']
+    if lightness > 0.85:
+        # Just make the light totally white
+        color['sat'] = 0
+    else:
+        # Amplify the saturation to make it pop
+        color['sat'] = min(1, color['sat'] + 0.2)
