0bc7b1da52569c2851c7217cba25ae98cf681f8c
Caleb Thorsteinson <caleb@thorsteinson.io>
2016-09-22 10:12:34 -0700

    Uses light resource instead of group resource
    The group resource is according to phillips, less performant. It only makes sense to use that resource when there are a dozen or more lights associated with that group. Since I'm only dealing with 3 lights, making multiple requests with the lights resource appears to be the better option, and the program will maintain the grouping for me.

--- phillips/core.py
+++ phillips/core.py
@@ -13,8 +13,8 @@ MAX_SAT = 254
 # Temp constants that we will eventually generate dynamically
 BRIDGE_IP = "10.0.0.217"
 USERNAME = "k-w0WCT6dJUfVudh506zzB-ejM2MPDoj0bntOPeB"
-PRIMARY_LIGHT_GROUP_ID = "2"
-SECONDARY_LIGHT_GROUP_ID = "3"
+PRIMARY_LIGHT_GROUP = [2, 3]
+SECONDARY_LIGHT_GROUP = [1]
 
 class Phillips:
     """A class for accessing light groups, and establishing a connection to the Phillips Hue Bridge"""
@@ -22,26 +22,31 @@ class Phillips:
         self.bridge_ip = BRIDGE_IP
         self.username = USERNAME
 
-    # Constructs the address for a hue group
-    def _construct_hue_url(self, group_id):
+    # Constructs the address for a hue light
+    def _construct_hue_url(self, light_id):
         return urljoin("http://" + self.bridge_ip,
-                        posixpath.join("api", self.username, "groups", group_id, "action"))
+                        posixpath.join("api", self.username, "lights", light_id, "action"))
     
     def get_secondary(self):
-        return LightGroup(self._construct_hue_url(SECONDARY_LIGHT_GROUP_ID))
+        urls = [self._construct_hue_url(id) for id in SECONDARY_LIGHT_GROUP]
+        return LightGroup(urls)
     def get_primary(self):
-        return LightGroup(self._construct_hue_url(PRIMARY_LIGHT_GROUP_ID))
+        urls = [self._construct_hue_url(id) for id in PRIMARY_LIGHT_GROUP]
+        return LightGroup(urls)
 
 class LightGroup:
     """A group of phillips lights that can have it's color altered"""
-    def __init__(self, url):
-        self.url = url
+    def __init__(self, light_urls):
+        # The urls for each light in the group. We avoid the groups API because it's more performant
+        # to directly access the lights themselves unless a group has at least a dozen lights
+        self.light_urls
     
     # Makes a request to change the color
     def change_color(self, color, brightness, transition_time):
         body = construct_hue_body(color, brightness, transition_time)
         payload = json.dumps(body)
-        r = requests.put(self.url, data=payload)
+        for url in self.light_urls:
+            requests.put(url, data=payload)
     
 # Takes a color and brightness then returns a map that will act the request body to phillips hue
 def construct_hue_body(color, brightness, transition_time):
